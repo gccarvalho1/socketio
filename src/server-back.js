b61da394623773b612d34068371a27f9e542cc14
gccarvalho1 <gabriel_carvalho16@hotmail.com>
2024-09-05 15:34:37 -0300

    FUNCIONANDO TUDO! FIM DE MODULO

--- src/server-back.js
+++ src/server-back.js
@@ -1,23 +1,56 @@
-import { atualizaDocumento, encontrarDocumento } from './documentosDB.js';
+import {
+    atualizaDocumento,
+    emitirDocumentos,
+    encontrarDocumento,
+    excluirDocumento,
+    inserirDocumento,
+} from './documentosDB.js';
 import io from './server.js';
 
 io.on('connection', (socket) => {
-    console.log('Um cliente se conectou com o id: ' + socket.id);
+    socket.on('emitir_documentos', async (devolverDocumentos) => {
+        const resultado = await emitirDocumentos();
+        devolverDocumentos(resultado);
+    });
+
+    socket.on('adicionar_documento', async (nomeDocumento) => {
+        const documentoExiste =
+            (await encontrarDocumento(nomeDocumento)) !== null;
+
+        if (documentoExiste) {
+            socket.emit('documento_existente', true);
+        } else {
+            socket.emit('documento_existente', false);
+            const documento = await inserirDocumento(nomeDocumento);
+
+            if (documento.acknowledged) {
+                io.emit('adicionar_documento_interface', nomeDocumento);
+            }
+        }
+    });
+
     socket.on('texto_documento', async (nomeDocumento, devolverDocumento) => {
         socket.join(nomeDocumento);
         const documento = await encontrarDocumento(nomeDocumento);
-        if (documento && documento.texto != '') {
+        if (documento) {
             devolverDocumento(documento.texto);
         }
     });
 
-    socket.on('texto_editor', ({ texto, nomeDocumento }) => {
-        // socket.broadcast.emit('texto_editor_clientes', texto);
-        const atualizacaoDocumento = atualizaDocumento(texto, nomeDocumento);
+    socket.on('texto_editor', async ({ texto, nomeDocumento }) => {
+        const atualizacaoDocumento = await atualizaDocumento(texto, nomeDocumento);
         if (atualizacaoDocumento.modifiedCount) {
-            // documento.texto = texto;
-            // console.log(documento.texto);
             io.to(nomeDocumento).emit('texto_editor_clientes', texto);
         }
     });
+
+    socket.on('excluir_documento', async (nome) => {
+        const resultado = await excluirDocumento(nome);
+
+        if (resultado.deletedCount) {
+            io.emit('excluir_documento_sucesso', nome);
+        }
+    });
+
+    // Parte do indexHTML
 });
